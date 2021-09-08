1960b95a093c970c90bd45ee4192fdb090b66a99
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-07 23:17:56 -0300

    Propriedades do React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Counter from './components/Counter';
 import RepositoryList from './components/RepositoryList';
 
 export default function App() {
   return (
-    <div>
+    <>
       <RepositoryList />
-    </div>
+      <Counter />
+    </>
   );
 }
